de058576c6c29b2f62954b409a3929f2f0296807
Kurisu Snowdeng <b_aquarius@163.com>
2016-01-06 22:11:50 +0800

    udate

--- poj/1006.cpp
+++ poj/1006.cpp
@@ -3,6 +3,23 @@
 using namespace std;
 
 int main() {
-
+	int p, e, i, d;
+	cin >> p >> e >> i >> d;
+	int cnt = 0;
+	while (p >= 0) {
+		cnt++;
+		int ans = 0;
+		for (int j = 1; j <= 21252; j++) {
+			int x = (d + j - p) % 23;
+			int y = (d + j - e) % 28;
+			int z = (d + j - i) % 33;
+			if (x == 0 && y == 0 && z == 0) {
+				ans = j;
+				break;
+			}
+		}
+		printf("Case %d: the next triple peak occurs in %d days.\n", cnt, ans);
+		cin >> p >> e >> i >> d;
+	}
 	return 0;
 }
